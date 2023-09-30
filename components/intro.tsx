f54500cb313cce209dc706900f9cfd6299ff8386
Akylbek04 <zamirovakylbek920@gmail.com>
2023-09-30 18:33:34 +0200

    react intersection observer addded

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,10 +7,16 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
   return (
     <section
+      ref={ref}
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
@@ -26,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="https://media.licdn.com/dms/image/D4D03AQGq0DBMh_2UvQ/profile-displayphoto-shrink_800_800/0/1680640508181?e=2147483647&v=beta&t=XWr7sUR_hH7F1pzyjAp3EkhIW_tveK8ipxllWVcD-JI"
-              alt="Akylbek portrait"
+              alt="Ricardo portrait"
               width="192"
               height="192"
               quality="95"
@@ -56,11 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Akylbek.</span> I'm a{" "}
-        <span className="font-bold">frontend developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React</span>.
+        <span className="underline">React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
@@ -74,6 +80,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
@@ -90,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/akylbek-zamirov/"
+          href="https://linkedin.com"
           target="_blank"
         >
           <BsLinkedin />
@@ -98,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/akylbek04"
+          href="https://github.com"
           target="_blank"
         >
           <FaGithubSquare />
