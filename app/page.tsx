968cdd57d189df072f5de7cf085d1ee1856a7a96
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-01 15:16:20 +0200

    added footer section and fixed bug from experience section.

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <SectionDivider/>
-      <About/>
-      <Projects/>
-      <Skills/>
-      <Experience/>
-      <Contact/>
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
     </main>
   );
 }
