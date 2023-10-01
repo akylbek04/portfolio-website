0b8dab416007cd7b006484509e2a5cdf5538b8f6
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-01 10:38:57 +0200

    added dark mode feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,16 +27,16 @@ export default function RootLayout({
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-        {/* <ThemeContextProvider> */}
+        <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
             {children}
             <Footer />
 
             <Toaster position="top-right" />
-            {/* <ThemeSwitch /> */}
+            <ThemeSwitch />
           </ActiveSectionContextProvider>
-        {/* </ThemeContextProvider> */}
+        </ThemeContextProvider>
       </body>
     </html>
   );
