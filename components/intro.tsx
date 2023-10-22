b08e63c8327acaf4bf4d60444c90a67600f2e5a9
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-22 23:01:18 +0200

    added and fixed code

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
+import cv from "@/public/CV.png";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
@@ -91,7 +92,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="CV.pdf"
           download
         >
           Download CV{" "}
@@ -100,7 +101,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/akylbek-zamirov/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +109,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/akylbek04"
           target="_blank"
         >
           <FaGithubSquare />
