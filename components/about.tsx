fa8296d5650a8132039e0007607a2879406b70e2
Akylbek04 <zamirovakylbek920@gmail.com>
2023-10-31 11:16:11 +0100

    edited about_me section

--- components/about.tsx
+++ components/about.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import Image from "next/image";
+import about from "@/public/about.jpg";
 
 export default function About() {
   const { ref } = useSectionInView("About");
@@ -11,39 +13,44 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-screen-xl mx-auto text-left flex flex-col md:flex-row items-center justify-center leading-8 sm:mb-40 "
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about"
     >
-      <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After transitioning from a <span className="font-medium">Biology</span>{" "}
-        major to <span className="font-medium">Computer Science</span>, I
-        wholeheartedly embraced my passion for programming. Through a coding
-        bootcamp, I cultivated a strong foundation in{" "}
-        <span className="font-medium"> web development</span>, where my
-        favorite aspect is the thrill of problem-solving and crafting elegant
-        solutions.
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My expertise
-        lies in{" "}
-        <span className="font-medium">React, Javascript(Typescript)</span>. I am
-        also familiar with Next.js, Node.js and MongoDB. My commitment to
-        continuous growth is unwavering. I recently completed a certification
-        course in <span className="font-medium">Typescript</span>. I am excited to
-        contribute my problem-solving prowess and creativity to a dynamic
-        software development team in a full-time role.
-      </p>
-      <p>
-        <span className="italic">Beyond coding</span>, I had diverse interests,
-        such as chess, football, and reading, fueling my curiosity for the human
-        mind's intricacies. I'm also learning the art of ping pong. I believe
-        that these diverse pursuits enhance my adaptability and creativity,
-        qualities I'm eager to bring to the table as a developer.
-      </p>
+      <div className="h-full flex w-full bg-gray-100 rounded-lg dark:bg-clip-padding dark:backdrop-filter dark:backdrop-blur-sm dark:bg-opacity-10">
+        <div className="w-full md:w-1/2 p-10 pe-5">
+          <h2 className="text-3xl font-bold mb-6 md:mb-8 text-gray-900 dark:text-gray-100">
+            About me
+          </h2>
+          <p className="text-lg mb-6 md:mb-8 text-gray-800 dark:text-gray-300">
+            Hello, I'm Akylbek Zamirov, a detail-oriented Frontend Developer
+            passionate about creating exceptional digital experiences. With
+            expertise in React, JavaScript and HTML&CSS, I enjoy simplifying
+            intricate concepts into user-friendly designs. Let's work together
+            on something extraordinary.
+          </p>
+          <p className="text-lg mb-6 md:mb-8 text-gray-800 dark:text-gray-300">
+            During my time at Cargo Fleet, I led the development of an advanced
+            Fleet Management application, streamlining document handling and
+            enhancing real-time tracking. Empowering efficient decision-making
+            and seamless operations, the platform optimized efficiency within
+            the trucking industry.
+          </p>
+          <p className="text-lg mb-6 md:mb-8 text-gray-800 dark:text-gray-300">
+            Currently, I'm actively seeking new opportunities in
+            Fullstack/Frontend positions.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2 flex justify-end p-10 ps-5">
+          <Image
+            src={about}
+            alt="about_jpg"
+            className="max-w-3/4 md:max-w-full h-auto rounded-md"
+          />
+        </div>
+      </div>
     </motion.section>
   );
 }
